0bdb83cdaf7683687248208c19140aacb9252bf3
Silpakorumban <102651370+Silpakorumban@users.noreply.github.com>
2022-04-21 00:22:56 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -23,6 +23,9 @@ function validate() {
     var Regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     var regp = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    var regp1 = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,10}$/;
+    var regp2 = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{10,12}$/;
+    var regp3 = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{12,16}$/;
 
     var regph = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
@@ -36,7 +39,7 @@ function validate() {
         alert("invalid phone no");
     }
 
-    if (!regp.test(pwd.value)) {
+    if (!(regp.test(pwd.value))) {
         alert("Password Not in Valid Format, it should contain a Minimum of 8 characters and a maximum of 16 characters, Must have one uppercase, lower case, number and special Characters");
         error.innerHTML = "invalid";
         error.style.paddingLeft = "200px";
@@ -47,6 +50,47 @@ function validate() {
         return false;
 
     }
+
+
+
+
+    if (regp3.test(pwd.value)) {
+        alert("valid");
+        error.innerHTML = "strong";
+        error.style.paddingLeft = "200px";
+        error.style.paddingBottom = "10px";
+        error.style.fontSize = "large";
+        error.style.color = "green";
+
+
+    }
+    if (regp2.test(pwd.value)) {
+        alert("valid");
+        error.innerHTML = "medium";
+        error.style.paddingLeft = "200px";
+        error.style.paddingBottom = "10px";
+        error.style.fontSize = "large";
+        error.style.color = "Orange";
+
+
+
+    }
+
+    if (regp1.test(pwd.value)) {
+        alert("valid");
+        error.innerHTML = "week";
+        error.style.paddingLeft = "200px";
+        error.style.paddingBottom = "10px";
+        error.style.fontSize = "large";
+        error.style.color = "yellow";
+
+
+
+    }
+
+
+
+
 
 
     if (!(pwd.value === pwd1.value)) {
